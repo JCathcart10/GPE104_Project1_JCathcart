460f956084cca15a336b2ffa7a3af684f8a5cffa
Jacob Cathcart <jaccathc@uat.edu>
2019-09-30 16:59:19 -0700

    v1.0

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,27 +6,73 @@ public class PlayerMovement : MonoBehaviour
 {
     // Declaring variables
     private Rigidbody2D rb;// makes rigidbody variable
-    public float thrust = 10.0f;// sets acceleration, can be set from inspector
-    public float moveSpeed;
+    public bool canMove;// bool to show that object can move; starts as true
+    public float speed = 1.0f;
 
     void Start()//happens on first frame
     {
         rb = GetComponent<Rigidbody2D>();// assigns the Rigidbody to the rb variable
+        
        // gameObject.transform.Translate(0.0f, 0.0f, 0.0f);// resest position to starting point.
     }
 
-    void FixedUpdate()//happens every frame
+    void Update()//happens every frame
     {
-        //if (Input.GetKeyDown(KeyCode.RightArrow))// hold right arrow to go right
-        //{
-        //    Debug.Log("right arrow is down");//testing input
-        //    Vector3 newVelocity = new Vector3(1, rb.velocity.y, 0);
-        //    rb.velocity = newVelocity;
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
-        //}
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, 0));
+        if (Input.GetButtonDown("space"))// detects when the player presses the space bar
+        {
+            gameObject.SetActive (false);// disables the game object
+        }
+
+
+
+        if (Input.GetButtonDown("pause"))// detects when the player presses the pause key (bound to 'p')
+        {
+            if (canMove == true)// checks if the canMove bool it true. if it is...
+            {
+                canMove = false;// ...it makes it false
+            }
+           else// if canMove is not true...
+            {
+                canMove = true;// it makes it true
+            }
+        }
+
+        if (canMove == true)// checks if canMove is true
+        {
+            if (Input.GetButton("shift"))// detects when the shift key is held down
+            {
+                if (Input.GetKeyDown(KeyCode.RightArrow))// checks for right arrow input; triggers once per key press
+                {
+                    transform.Translate(1.0f, 0.0f, 0.0f);// moves 1 unit to the right
+                }
+
+                if (Input.GetKeyDown(KeyCode.LeftArrow))// checks for left arrow input; triggers once per key press
+                {
+                    transform.Translate(-1.0f, 0.0f, 0.0f);// moves 1 unit to the left
+                }
+
+                if (Input.GetKeyDown(KeyCode.UpArrow))// checks for up arrow input; triggers once per key press
+                {
+                    transform.Translate(0.0f, 1.0f, 0.0f);// moves 1 unit up
+                }
+
+                if (Input.GetKeyDown(KeyCode.DownArrow))// checks for down arrow input; triggers once per key press
+                {
+                    transform.Translate(0.0f, -1.0f, 0.0f);// moves 1 unit down
+                }
+            }
+
+            else if (Input.GetButtonDown("reset"))// if the q key is pressed down...
+            {
+                transform.position = Vector3.zero;// the position is reset to (0,0,0)
+            }
+
+            else// if the shift or q keys are not held down
+            {
+                transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0));
+                // moves the sprite based on the the input from the arrow keys. the speed can be modified from the inspector
+            }
+
+        }
     }
 }
---
+++ Assets/Scripts/quit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("quit"))// detects when the player presses esc
+        {
+            Application.Quit();// quits game
+        }
+    }
+}
